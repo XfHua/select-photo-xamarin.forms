74e15e050f5f190d0af3e71f9714d326b1d95c32
XfHua <jasonHuaxf@163.com>
2020-01-28 10:22:12 +0800

    Add files via upload

---
+++ Xamarin_Forms___Dependency_Service/DependencyServiceDemos/MainPage.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using Xamarin.Forms;
+
+namespace DependencyServiceDemos
+{
+    public partial class MainPage : TabbedPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            myViewModel model = new myViewModel();
+
+            this.BindingContext = model;
+        }
+    }
+
+
+    public class myViewModel : INotifyPropertyChanged {
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public Command SelectPictureCommand { get; }
+
+        public ImageSource itemPic { get; set; }
+
+
+        public ImageSource ItemPic
+        {
+            set
+            {
+                if (itemPic != value)
+                {
+                    itemPic = value;
+
+                    if (PropertyChanged != null)
+                    {
+                        PropertyChanged(this, new PropertyChangedEventArgs("ItemPic"));
+                    }
+                }
+            }
+            get
+            {
+                return itemPic;
+            }
+        }
+
+        public myViewModel() {
+
+            SelectPictureCommand = new Command(execute: async () =>
+            {
+                //if (IsBusy)
+                //{
+                //    return;
+                //}
+
+                //IsBusy = true;
+
+                try
+                {
+                    Stream stream = await DependencyService.Get<IPhotoPickerService>().GetImageStreamAsync();
+
+                    if (stream != null)
+                    {
+                        ItemPic = ImageSource.FromStream(() => stream);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine(ex);
+                }
+                finally
+                {
+                    //IsBusy = false;
+                }
+            });
+        }
+    }
+}
